63c990921b51f899e29219c42cb553764f951424
ilg2005 <igoldfarb10@gmail.com>
2018-07-04 02:36:15 +0300

    исправлена функция getPinMainCoordinates

--- js/map.js
+++ js/map.js
@@ -266,10 +266,9 @@ var togglePageState = function (state) {
 };
 
 var getPinMainCoordinates = function () {
-  var elementCoordinates = pinMainElement.getBoundingClientRect();
   var pinMainCoordinates = {
-    x: Math.round(elementCoordinates.left + elementCoordinates.width / 2),
-    y: Math.round(elementCoordinates.bottom)
+    x: Math.round(pinMainElement.offsetLeft - pinMainElement.offsetWidth / 2),
+    y: Math.round(pinMainElement.offsetTop - pinMainElement.offsetHeight)
   };
   return pinMainCoordinates;
 };
