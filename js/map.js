ac3e89e3c636e5cc1233df3a8b3c783ba043e34d
ilg2005 <igoldfarb10@gmail.com>
2018-07-08 18:14:45 +0300

    выделен модуль form.js

--- js/map.js
+++ js/map.js
@@ -28,13 +28,6 @@ var HOUSING_TYPES = {
   'bungalo': 'Бунгало'
 };
 
-var HOUSING_MIN_PRICES = {
-  'palace': '10000',
-  'flat': '1000',
-  'house': '5000',
-  'bungalo': '0'
-};
-
 var CONTROL_HOURS = ['12:00', '13:00', '14:00'];
 
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
@@ -69,12 +62,6 @@ var pinTemplateElement = templateElement.querySelector('.map__pin');
 var formFieldsElement = document.querySelectorAll('fieldset');
 var pinMainElement = document.querySelector('.map__pin--main');
 var formAddressElement = document.querySelector('#address');
-var formHousingTypeElement = document.querySelector('#type');
-var formPriceElement = document.querySelector('#price');
-var formCheckinElement = document.querySelector('#timein');
-var formCheckoutElement = document.querySelector('#timeout');
-var formRoomsQuantityElement = document.querySelector('#room_number');
-var formGuestsQuantityElement = document.querySelector('#capacity');
 var popupCloseElement;
 
 var getNeedlepointCoordinates = function (PIN_SIZE, pinElement) {
@@ -286,18 +273,12 @@ var init = function () {
   var initialNeedlepointCoordinates = getNeedlepointCoordinates(MAIN_PIN_SIZE, pinMainElement);
   insertNeedlepointAddress(initialNeedlepointCoordinates);
   cards = generateCards(CARDS_QUANTITY);
-  formPriceElement.setAttribute('min', HOUSING_MIN_PRICES[formHousingTypeElement.value]);
 };
 init();
 
 var onPinMousedown = function (evtDown) {
   togglePageState(activeState);
   renderSimilarPins(document.querySelector('.map__pins'));
-  formHousingTypeElement.addEventListener('change', onFormHousingTypeElementChange);
-  formCheckinElement.addEventListener('change', onFormCheckinElementChange);
-  formCheckoutElement.addEventListener('change', onFormCheckoutElementChange);
-  formRoomsQuantityElement.addEventListener('change', onFormRoomsQuantityElementChange);
-  formGuestsQuantityElement.addEventListener('change', onFormGuestsQuantityElementChange);
 
   var startMouseCoordinates = {
     x: evtDown.x,
@@ -366,46 +347,3 @@ document.addEventListener('click', function (evt) {
     document.addEventListener('keydown', onPopupClose);
   }
 });
-
-var onFormHousingTypeElementChange = function () {
-  setHousingMinPrice(formHousingTypeElement.value);
-};
-
-var setHousingMinPrice = function (selectedHousingType) {
-  formPriceElement.setAttribute('placeholder', HOUSING_MIN_PRICES[selectedHousingType]);
-  formPriceElement.setAttribute('min', HOUSING_MIN_PRICES[selectedHousingType]);
-};
-
-var onFormCheckinElementChange = function () {
-  formCheckoutElement.value = formCheckinElement.value;
-};
-
-var onFormCheckoutElementChange = function () {
-  formCheckinElement.value = formCheckoutElement.value;
-};
-
-var setCapacityLimitations = function (roomsQuantity, guestsQuantity) {
-  formGuestsQuantityElement.setCustomValidity('');
-  roomsQuantity = Number(roomsQuantity);
-  guestsQuantity = Number(guestsQuantity);
-  if (roomsQuantity === 1 && guestsQuantity !== 1) {
-    formGuestsQuantityElement.setCustomValidity('1 комната - для 1 гостя');
-  } else if (roomsQuantity === 2 && guestsQuantity !== 1 && guestsQuantity !== 2) {
-    formGuestsQuantityElement.setCustomValidity('2 комнаты - для 1 или 2 гостей');
-  } else if (roomsQuantity === 3 && guestsQuantity !== 1 && guestsQuantity !== 2 && guestsQuantity !== 3) {
-    formGuestsQuantityElement.setCustomValidity('3 комнаты - для 1, 2 или 3 гостей');
-  } else if (roomsQuantity === 100 && guestsQuantity !== 0) {
-    formGuestsQuantityElement.setCustomValidity('100 комнат - не для гостей');
-  } else {
-    formGuestsQuantityElement.setCustomValidity('');
-  }
-};
-
-var onFormRoomsQuantityElementChange = function () {
-  setCapacityLimitations(formRoomsQuantityElement.value, formGuestsQuantityElement.value);
-};
-
-var onFormGuestsQuantityElementChange = function () {
-  setCapacityLimitations(formRoomsQuantityElement.value, formGuestsQuantityElement.value);
-};
-
